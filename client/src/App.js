fd8fe0e785fd469fa0ca9c59703253bed58df3dc
Nguyen Lam <nguyenlam@MacBook-Pro-14.local>
2023-04-11 11:55:38 +0700

    change page routers

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,23 @@
-import logo from './logo.svg';
 import './assets/styles/fonts.css'
 import "./assets/fontawesome"
 import './App.css';
-import Main from './components/layout/Main/Main';
 import Home from './page/Home';
+import { Route, Routes } from 'react-router-dom';
+import Login from './page/Login';
+import Register from './page/Register';
+import ProvideContactDetails from './page/ProvideContactDetails';
+import ProvidePassengerDetails from './page/ProvidePassengerDetails';
 
 function App() {
   return (
     <div className="App">
-      <Home />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/register' element={<Register />} />
+        <Route path='/contact-detail' element={<ProvideContactDetails />} />
+        <Route path='/passenger-detail' element={<ProvidePassengerDetails />} />
+      </Routes>
     </div>
   );
 }
