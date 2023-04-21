4377a54e9f499a7cedfc5fc7afef881396fb2806
phuongtrinh547 <truongtrinh547@gmail.com>
2023-04-21 14:22:54 +0700

    edit home page

--- client/src/page/Home/index.js
+++ client/src/page/Home/index.js
@@ -1,10 +1,6 @@
 import React, { useEffect, useState } from 'react';
-import panelImage from '../../assets/home/1760844.webp';
 import homeIcon from '../../assets/home/Rectangle 4240.png';
 import arrowIcon from '../../assets/home/arr.png';
-import calendarIcon from '../../assets/home/Calendar.png';
-import arr1 from '../../assets/home/arr1.png';
-import arrowWhite from '../../assets/home/arrowWhite.png';
 import { DatePicker, Select, message } from 'antd';
 import './style.css'
 import { getProvince } from '../../services/province';
@@ -70,97 +66,97 @@ function Home() {
 	}
 
 	return (
-		<>
-			<div className='home'>
-				{/* panel */}
-				<div className='panel'>
-					<img src='images/leaderboard.png' alt='panel' />
-				</div>
-				{/* middle */}
+		<div className='home' style={{
+			backgroundImage: "url(images/leaderboard.png)"
+		}}>
+			{/* panel */}
+			<div className='panel'>
+				{/* <img src='images/leaderboard.png' alt='panel' /> */}
+			</div>
+			{/* middle */}
 
-				<ul className='flex flex-col items-center py-5 main' >
-					<div className='relative'>
-						<img
-							src={homeIcon}
-							alt='City Icon'
-							className='absolute left-0 top-0 h-6 ml-1 mt-2'
-							style={{ zIndex: 1 }}
-						/>
-						<Select
-							showSearch
-							placeholder='Source City'
-							onChange={handleSourceCitySelect}
-							value={sourceCity || undefined}
-							showArrow={false}
-						>
-							{cities ? cities.map(province => (
-								<Select.OptGroup key={province.provinceId} label={province.name}>
-									{province.location.map(city => (
+			<ul className='flex flex-col items-center py-5 main' >
+				<div className='relative'>
+					<img
+						src={homeIcon}
+						alt='City Icon'
+						className='absolute left-0 top-0 h-6 ml-1 mt-2'
+						style={{ zIndex: 1 }}
+					/>
+					<Select
+						showSearch
+						placeholder='Source City'
+						onChange={handleSourceCitySelect}
+						value={sourceCity || undefined}
+						showArrow={false}
+					>
+						{cities ? cities.map(province => (
+							<Select.OptGroup key={province.provinceId} label={province.name}>
+								{province.location.map(city => (
+									<Select.Option key={city.locationId} value={city.name}>
+										{city.name}
+									</Select.Option>
+								))}
+							</Select.OptGroup>
+						)) : ''}
+					</Select>
+				</div>
+				<li className='my-2 cursor-pointer' onClick={handleSwapClick}>
+					<img alt='error' src={arrowIcon}
+						className='w-12' style={{ margin: "10px 0px" }} />
+				</li>
+				<div
+					className='relative'
+				>
+					<img
+						src={homeIcon}
+						alt='City Icon'
+						className='absolute left-0 top-0 h-6 ml-1 mt-2'
+						style={{ zIndex: 1 }} />
+					<Select
+						showSearch
+						placeholder='Destination City'
+						onChange={handleDestinationCitySelect}
+						disabled={sourceCity === ''}
+						value={destinationCity || undefined}
+					>
+						{cities ? cities.map(province => (
+							<Select.OptGroup key={province.provinceId} label={province.name}>
+								{province.location
+									.filter((city) => city.name !== sourceCity)
+									.map((city) => (
 										<Select.Option key={city.locationId} value={city.name}>
 											{city.name}
 										</Select.Option>
 									))}
-								</Select.OptGroup>
-							)) : ''}
-						</Select>
+							</Select.OptGroup>
+						)) : ''}
+					</Select>
+				</div>
+				<li className='flex flex-col mt-2 calendar'>
+					<div className="relative flex items-center  rounded px-10 py-2 w-full cursor-pointer hover:opacity-75">
+						<FontAwesomeIcon icon={faCalendarDays} style={{ color: "#165F81" }} className=" absolute z-10 ml-2 w-5" />
+						<DatePicker className="flex-grow px-10 py-2 cursor-pointer hover:opacity-75"
+							value={searchDate}
+							onChange={handleDateChange}
+							disabledDate={disabledDate}
+							format="DD/MM/YYYY"
+							placeholder="Choose from calendar"
+							showTime={false}
+							suffixIcon={
+								<FontAwesomeIcon icon={faAngleRight} style={{ color: "#165f81" }} className=' absolute top-1 ml-4' />
+							}
+						/>
 					</div>
-					<li className='my-2 cursor-pointer' onClick={handleSwapClick}>
-						<img alt='error' src={arrowIcon}
-							className='w-12' style={{ margin: "10px 0px" }} />
-					</li>
-					<div
-						className='relative'
-					>
-						<img
-							src={homeIcon}
-							alt='City Icon'
-							className='absolute left-0 top-0 h-6 ml-1 mt-2'
-							style={{ zIndex: 1 }} />
-						<Select
-							showSearch
-							placeholder='Destination City'
-							onChange={handleDestinationCitySelect}
-							disabled={sourceCity === ''}
-							value={destinationCity || undefined}
-						>
-							{cities ? cities.map(province => (
-								<Select.OptGroup key={province.provinceId} label={province.name}>
-									{province.location
-										.filter((city) => city.name !== sourceCity)
-										.map((city) => (
-											<Select.Option key={city.locationId} value={city.name}>
-												{city.name}
-											</Select.Option>
-										))}
-								</Select.OptGroup>
-							)) : ''}
-						</Select>
-					</div>
-					<li className='flex flex-col mt-5 calendar'>
-						<div className="relative flex items-center  rounded px-10 py-2 w-full cursor-pointer hover:opacity-75">
-							<FontAwesomeIcon icon={faCalendarDays} style={{ color: "#165F81" }} className=" absolute z-10 ml-2 w-5" />
-							<DatePicker className="flex-grow px-10 py-2 cursor-pointer hover:opacity-75"
-								value={searchDate}
-								onChange={handleDateChange}
-								disabledDate={disabledDate}
-								format="DD/MM/YYYY"
-								placeholder="Choose from calendar"
-								showTime={false}
-								suffixIcon={
-									<FontAwesomeIcon icon={faAngleRight} style={{ color: "#165f81" }} className=' absolute top-1 ml-4' />
-								}
-							/>
-						</div>
-					</li>
-					{/* search button */}
+				</li>
+				{/* search button */}
 
-					<li className='btn-search mt-5' onClick={handleSearch}>
-						<button>SEARCH NOW</button>
-						<i className="arrow-icon"><FontAwesomeIcon icon={faArrowRight} /></i>
-					</li>
-				</ul >
-			</div >
-		</>
+				<li className='btn-search mt-5' onClick={handleSearch}>
+					<button>SEARCH NOW</button>
+					<i className="arrow-icon"><FontAwesomeIcon icon={faArrowRight} /></i>
+				</li>
+			</ul >
+		</div >
 	);
 }
 
