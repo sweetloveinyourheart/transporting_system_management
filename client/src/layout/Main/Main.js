dbc7f8eb08e38b66802d178b571d520db8639a75
Nguyen Lam <nguyenlam@MacBook-Pro-14.local>
2023-04-24 01:02:04 +0700

    check type error for routers

--- client/src/layout/Main/Main.js
+++ client/src/layout/Main/Main.js
@@ -3,12 +3,7 @@ import Footer from '../Footer/Footer'
 import './main.css'
 import Router from '../../routers'
 
-import { useAuth } from '../../contexts/auth'
-
-
 export default function Main() {
-
-    const { user } = useAuth();
     return (
         <div className='page-container'>
             <Header></Header>
