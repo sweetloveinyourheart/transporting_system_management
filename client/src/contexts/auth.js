2252c2cca6efa7cb2134626ab52f637ba17d953a
Tin Nguyen <nguyenbatin290900@gmail.com>
2023-04-24 19:44:51 +0700

    hotfix/file auth

--- client/src/contexts/auth.js
+++ client/src/contexts/auth.js
@@ -48,8 +48,13 @@ export default function AuthProvider({ children }) {
 			setLoading(false)
 			return setUser(null)
 		}
-		//If driver login navigate to driver page
+		if (user.role.roleId === "USER" && window.location.pathname.includes("/driver")) {
+			navigate("/")
+			setLoading(false)
+			return setUser(user)
+		}
 		if (user.role.roleId === "DRIVER") {
+
 			navigate("/driver/*")
 			setLoading(false)
 			return setUser(user)
